853e018787c155b29bfaf3fb5ec9945ec5c05a56
Christian Prior <cprior@gmail.com>
2018-08-13 20:30:12 +0200

    Added simple check for german or us-american workday.

--- application/physical/otmr/dispatcher/main.go
+++ application/physical/otmr/dispatcher/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
+	"github.com/rickar/cal"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
@@ -21,8 +22,9 @@ type Index struct {
 
 //Exchange holds the common abbreviation of a stock exchange and a slice of indices originating there.
 type Exchange struct {
-	Name    string
-	Indices []Index
+	Name     string
+	Calendar string
+	Indices  []Index
 }
 
 var (
@@ -46,38 +48,51 @@ func handle(w http.ResponseWriter, r *http.Request) {
 //issues an internal API call to fetch current data in json format
 func handleExchanges(w http.ResponseWriter, r *http.Request) {
 
-	ctx := appengine.NewContext(r)
-	endpoint = getEndpoint(appengine.ModuleName(ctx), appengine.AppID(ctx))
-	client := urlfetch.Client(ctx)
-
-	//http://localhost:8080/exchange/foo?bar=baz
-	//tmp := r.URL.Query()["bar"][0] //will be bar
-	//r.URL.Path // will be /exchange/foo
-
 	exchange := strings.Split(strings.TrimPrefix(r.URL.Path, "/exchange/"), "/")[0]
 	//exchanges := getAllExchanges()
 
-	foo, err := getExchange(ctx, exchange)
+	foo, err := getExchange(exchange)
 	if err != nil {
 		fmt.Fprintln(w, err)
 	}
+	//fmt.Fprintf(w, "Calendar is %v\n", foo.Calendar)
+	c := cal.NewCalendar()
+	if foo.Calendar == "de" {
+		cal.AddGermanHolidays(c)
+		cal.AddEcbHolidays(c)
+	} else if foo.Calendar == "us" {
+		cal.AddUsHolidays(c)
+	}
+	t := time.Now()
 
-	for _, index := range foo.Indices {
-		//https://stackoverflow.com/a/13826910/9576512
-		var URL *url.URL
-		URL, _ = url.Parse(getEndpoint(appengine.ModuleName(ctx), appengine.AppID(ctx)) + "TIME_SERIES_DAILY_ADJUSTED/" + index.Symbol)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+	if c.IsWorkday(t) {
+		ctx := appengine.NewContext(r)
+		endpoint = getEndpoint(appengine.ModuleName(ctx), appengine.AppID(ctx))
+		client := urlfetch.Client(ctx)
+
+		//http://localhost:8080/exchange/foo?bar=baz
+		//tmp := r.URL.Query()["bar"][0] //will be bar
+		//r.URL.Path // will be /exchange/foo
+
+		for _, index := range foo.Indices {
+			//https://stackoverflow.com/a/13826910/9576512
+			var URL *url.URL
+			URL, _ = url.Parse(getEndpoint(appengine.ModuleName(ctx), appengine.AppID(ctx)) + "TIME_SERIES_DAILY_ADJUSTED/" + index.Symbol)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			_, err := client.Get(URL.String())
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			//fmt.Fprintf(w, "HTTP GET returned status %v\n", resp.Status)
 		}
-
-		resp, err := client.Get(URL.String())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Fprintf(w, "HTTP GET returned status %v\n", resp.Status)
+	} else {
+		fmt.Fprintf(w, "Is not a workday %v\n", "")
 	}
 
 	fmt.Fprintln(w, "otmr-dev-dispatcher-v1/exchange")
@@ -88,7 +103,8 @@ func getAllExchanges() []*Exchange {
 
 	exchanges := []*Exchange{
 		&Exchange{
-			Name: "NASDAQ",
+			Name:     "NASDAQ",
+			Calendar: "us",
 			Indices: []Index{
 				Index{
 					Name:   "S&P",
@@ -97,7 +113,8 @@ func getAllExchanges() []*Exchange {
 			},
 		},
 		&Exchange{
-			Name: "XETRA",
+			Name:     "XETRA",
+			Calendar: "de",
 			Indices: []Index{
 				Index{
 					Name:   "DAX",
@@ -113,7 +130,7 @@ func getEndpoint(modulename string, appid string) string {
 	return "http://" + modulename + "-dot-" + appid + ".appspot.com/av/"
 }
 
-func getExchange(ctx context.Context, exchange string) (*Exchange, error) {
+func getExchange(exchange string) (*Exchange, error) {
 	exchanges := getAllExchanges()
 	for _, v := range exchanges {
 		if strings.ToLower(v.Name) == strings.ToLower(exchange) {
